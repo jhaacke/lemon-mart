e85e8bdd5308ed0944bc2e60a9fee34cd5829a00
Jeff Haacke <jeff@gobeyondthecube.com>
2018-08-23 10:48:52 -0400

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+  <mat-toolbar color="primary">
+    <button mat-icon-button><mat-icon>menu</mat-icon></button>
+    <mat-icon svgIcon="lemon"></mat-icon>
+    <a mat-button routerLink="/home"><h1>LemonMart</h1></a>
+    <span class="flex-spacer"></span>
+    <button mat-icon-button><mat-icon>account_circle</mat-icon></button>
+  </mat-toolbar>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'lemon-mart';
+
+  constructor(iconRegistry: MatIconRegistry, sanitizor: DomSanitizer) {
+    iconRegistry.addSvgIcon('lemon', sanitizor.bypassSecurityTrustResourceUrl('assets/img/icons/lemon.svg'));
+  }
 }
